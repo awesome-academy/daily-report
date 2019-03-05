6b5850d5527915798bb251dd4e850963e574e8ab
memsenpai <memsenpai3@gmail.com>
2019-03-05 21:43:09 +0700

    cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
+  config.web_socket_server_url = "wss://#{ENV['RAILS_HOST']}/cable"
+  config.action_cable.allowed_request_origins = ENV['RAILS_HOST']
   config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
